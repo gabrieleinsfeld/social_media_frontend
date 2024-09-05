c16fa119a5faaa8ae68962d0821a2fd56642e994
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-09-05 15:36:26 -0300

    Deleted the files related to examples

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
-import exampleReducer from "./exampleReducer";
 import userReducer from "./userReducer";
 import postReducer from "./postReducer";
 const rootReducer = combineReducers({
-  example: exampleReducer,
   user: userReducer,
   posts: postReducer,
 });
